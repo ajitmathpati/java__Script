fb40d8eb0e1272e88fbc6506b0c51914c0d75cad
ajitmathpati <ajitmathpati211@gmail.com>
2025-04-27 19:14:24 +0530

    for each loop

---
+++ objectinarray.js
@@ -0,0 +1,19 @@
+const MYArray = [
+    {
+        name: "ajit",
+        age: 21,
+    },
+    {
+        name: "neha",
+        age: 21,
+    },
+    {
+        name: "sumit",
+        age: 23,
+    },
+];
+MYArray.forEach((value,index,MYArray)=> {
+
+    console.log(MYArray);
+    
+})
