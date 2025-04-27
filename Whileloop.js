6e8ba6413729ece7866c5c9b7578bbb2adfb068e
ajitmathpati <ajitmathpati211@gmail.com>
2025-04-27 12:46:20 +0530

    while problem

---
+++ Whileloop.js
@@ -0,0 +1,9 @@
+const Loop = () => {
+    const myArray = ["ajit", "sujit", "sachin", "sandip"];
+    let i = 0;
+    while (i < myArray.length) {
+        console.log(`ARRYA NAMES ARE :${myArray[i]}`);
+        i++;
+    }
+}
+Loop();
