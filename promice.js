f33191164760a59c6dcf582047ac693c2320d9dd
ajitmathpati <ajitmathpati211@gmail.com>
2025-05-17 14:20:24 +0530

    add

--- promice.js
+++ promice.js
@@ -15,13 +15,27 @@
 // })
 
 
-new Promise((resolve, reject) => {
+// new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         console.log('HI I AM MATHPATI');
+//         resolve()
+
+//     }, 1000);
+// }).then(() => {
+//     console.log('async 2 resolve');
+
+// })
+
+const promisethree = new Promise((resolve, reject) => {
     setTimeout(() => {
-        console.log('HI I AM MATHPATI');
-        resolve()
+        console.log('hi ');
+        resolve({ name: "ajit ", email: "ajit mathpatii 211@ gmail.com" })
 
     }, 1000);
-}).then(() => {
-    console.log('async 2 resolve');
+
+})
+promisethree.then(({ name ,email }) => {
+    console.log(name ,email);
+
 
 })
