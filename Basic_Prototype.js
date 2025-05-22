74e5c7f6167f45f820e78d59923c6fd3f036bece
ajitmathpati <ajitmathpati211@gmail.com>
2025-05-22 17:35:18 +0530

    add

--- Basic_Prototype.js
+++ Basic_Prototype.js
@@ -1,9 +1,31 @@
-const mutiplication = (num) => {
-    return num * 5;
+// const mutiplication = (num) => {
+//     return num * 5;
+
+// }
+// mutiplication.power=2;
+// console.log(mutiplication(5));
+// console.log(mutiplication.power);
+// console.log(mutiplication.prototype);
+
+
+function createuser(myname, score) {
+    this.myname = myname;
+    this.score = score;
+}
+createuser.prototype.increment = function () {
+    this.score++;
+}
+createuser.prototype.printme = function () {
+    console.log(`my score is ${this.score}`);
 
 }
-mutiplication.power=2;
-console.log(mutiplication(5));
-console.log(mutiplication.power);
-console.log(mutiplication.prototype);
+
+
+const coffee =new createuser('coffee', 20);
+const chai = new createuser('tea', 50);
+coffee.increment();
+
+coffee.printme();
+chai.increment();
+chai.printme();
 
