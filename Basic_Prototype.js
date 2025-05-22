b0adcc3e41a007d28d090b3562ef9767400837f1
ajitmathpati <ajitmathpati211@gmail.com>
2025-05-22 16:58:25 +0530

    prototype

---
+++ Basic_Prototype.js
@@ -0,0 +1,9 @@
+const mutiplication = (num) => {
+    return num * 5;
+
+}
+mutiplication.power=2;
+console.log(mutiplication(5));
+console.log(mutiplication.power);
+console.log(mutiplication.prototype);
+
