717215079003c382ec291fda0ba42d52b9e9a52e
ajitmathpati <ajitmathpati211@gmail.com>
2025-05-23 17:45:40 +0530

    add

--- inheritance.js
+++ inheritance.js
@@ -2,6 +2,10 @@ class user {
     constructor(name) {
         this.name = name;
     }
+    logme() {
+        console.log(`hello ${this.name}`);
+
+    }
 }
 class admin extends user {
     constructor(name, email, password) {
@@ -11,9 +15,25 @@ class admin extends user {
 
     }
     addcourese() {
-        console.log(`couser add by ${this.name}`);
+        console.log(`i am admin${this.name}`);
+
+    }
+}
+class techer extends user {
+    constructor(name, email, password) {
+        super(name);
+        this.email = email;
+        this.password = password;
+
+    }
+    addcourese() {
+        console.log(`i am techer ${this.name}`);
 
     }
 }
 const chai = new admin('ajit', 'ajit@123', '12345');
+const sujit=new techer('sujit ','sujit@1233','3232');
+sujit.addcourese();
 chai.addcourese();
+// let ajit = new user('sujit');
+// ajit.logme();
